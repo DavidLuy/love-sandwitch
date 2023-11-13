bafaa9d37a33cc6faed472f2ba0bc93e59ec96ab
DavidLuy <>
2023-11-13 11:31:02 +0000

    Add initial commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service_account import credentials
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -7,13 +7,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-    CREDS = Credentials.from_service_account_file('creds.json')
-    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open('love_sandwich')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwich')
 
-    sales = SHEET.worksheet('sales')
+sales = SHEET.worksheet('sales')
 
-    data = sales.get_all_values()
+data = sales.get_all_values()
 
-    print(data)
+print(data)
